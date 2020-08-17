92e133592b2958b2164c1e1b57ebbe75141a9f29
Katie Poa <kpoa@berkeley.edu>
2020-08-17 16:50:31 -0700

    Completed getElementsByClassName.js (passes all tests)! - passed in three arguments (className, node, result)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,22 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node, result) {
+  node = node || document.body;
+  // make final result array
+  result = result || [];
+  // parse through each element in html
+  // if element classlist contains class name
+  if (node.classList.contains(className)) {
+    // push to result array
+    result.push(node);
+  }
+  // base case
+  // iterate through child result
+  for (var i = 0; i < node.children.length; i++) {
+    // recusively call function on each element  }
+    getElementsByClassName(className, node.children[i], result);
+  }
+  // return result array
+  return result;
 };
