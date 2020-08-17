5871cc7b0ebfc28291cba2281efd0ae2151f9487
Katie Poa <kpoa@berkeley.edu>
2020-08-17 16:02:01 -0700

    Completed stringifyJSON (passes all tests)

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,36 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+// input - value
+// output - string version of value
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // if obj is a string
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    var results = [];
+    //iterate through, recursive call each element
+    for (var i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+    }
+    return '[' + results.join(',') + ']';
+  }
+
+  if (obj && typeof obj === 'object') {
+    if (obj === undefined || typeof obj === 'function') {
+      return '{}';
+    }
+    var results = [];
+    //iterate and recursive call
+    for (var key in obj) {
+      if (obj[key] !== undefined && typeof(obj[key]) !== 'function') {
+        results.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    return '{' + results.join(',') + '}';
+  }
+
+  return '' + obj;
 };
