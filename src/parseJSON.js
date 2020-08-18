9cf9ede3bb48ca5994887b3f9be1fae3c1f47414
Katie Poa <kpoa@berkeley.edu>
2020-08-17 22:06:41 -0700

    Implemented parseJSON.js to pass first 4 tests

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,41 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  var elements = json.split('');
+  if (elements[0] === '[') {
+    var results = [];
+    for (var i = 0; i < elements.length; i ++) {
+      if (elements[i] !== '"' && elements[i] !== '[' && elements[i] !== ']') {
+        results.push(elements[i]);
+      }
+    }
+    return results;
+  }
+  if (elements[0] === '{') {
+
+    // create empty results obj
+    var results = {};
+    // iterate through elements
+    for (var i = 0; i < elements.length; i++) {
+      // if elements[i] === ':'
+      // slice up to the index of the colon and set that as key
+      // if (!elements.contains(',') && !elements[i] === ',' && )
+      if (elements[i] === ':') {
+        var key = elements.slice((elements.indexOf('{') + 2), (i - 1));
+        key = key.join('');
+        // slice from colon to next comma and set that the value
+        var value = elements.slice((i + 3), (elements.length - 2));
+        value = value.join('');
+        results[key] = value;
+        return results;
+      }
+    //     var key = elements.slice((elements.indexOf('{') + 2), (elements.indexOf(':') - 1));
+    //     // slice from colon to next comma and set that the value
+    //     var value = elements.slice((elements.indexOf(':') + 1), (i - 1));
+    //     results[key] = value;
+    //     parseJSON( '{' + elements.slice(i) );
+    //   }
+    }
+    return results;
+  }
 };
